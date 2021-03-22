c2557b8faa859c5ea91d97b77844e5fe61e4d736
Febrilian Kristiawan <febriliankr@gmail.com>
2021-03-22 12:17:15 +0700

    deploying to heroku

---
+++ go.mod
@@ -0,0 +1,18 @@
+module web-scraping
+
+go 1.16
+
+require (
+	github.com/0xAX/notificator v0.0.0-20191016112426-3962a5ea8da1 // indirect
+	github.com/codegangsta/envy v0.0.0-20141216192214-4b78388c8ce4 // indirect
+	github.com/codegangsta/gin v0.0.0-20171026143024-cafe2ce98974 // indirect
+	github.com/cosmtrek/air v1.21.2 // indirect
+	github.com/creack/pty v1.1.11 // indirect
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/gocolly/colly/v2 v2.0.0
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/mattn/go-shellwords v1.0.11 // indirect
+	github.com/pelletier/go-toml v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
+	gopkg.in/urfave/cli.v1 v1.20.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gocolly/colly/v2"
+)
+
+// http://localhost:8080/search?url=http://paulgraham.com/articles.html
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	log.Println("Ping")
+	w.Write([]byte("ping"))
+}
+
+func main() {
+
+	addr := ":8080"
+
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/search", getData)
+	http.HandleFunc("/ping", ping)
+	log.Println("listening on ", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+type hyperlink struct {
+	href string
+	text string
+}
+
+func getData(w http.ResponseWriter, r *http.Request) {
+	//Verify the param "URL" exists
+	URL := r.URL.Query().Get("url")
+	if URL == "" {
+		log.Println("missing URL argument")
+		return
+	}
+	log.Println("visiting", URL)
+
+	//Create a new collector which will be in charge of collect the data from HTML
+	c := colly.NewCollector()
+
+	//Slices to store the data
+	var response []hyperlink
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Request.AbsoluteURL(e.Attr("href"))
+		text := e.Text
+
+		hl := hyperlink{
+			href: link,
+			text: text,
+		}
+
+		if link != "" && text != "" {
+			response = append(response, (hl))
+		}
+	})
+
+	c.Visit(URL)
+
+	log.Println(response)
+
+	object, err := json.Marshal(response)
+	if err != nil {
+		log.Println("failed to serialize response:", err)
+		return
+	}
+	// Add some header and write the body for our endpoint
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(object)
+}
